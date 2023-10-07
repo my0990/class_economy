1b716285ec410e2ec4ef46c8672f04167ee61df4
my0990 <my0990@naver.com>
2023-10-08 07:19:25 +0900

    test

--- app/test/[id]/page.js
+++ app/test/[id]/page.js
@@ -1,14 +1,14 @@
-'use client'
+
 import { useQRCode } from "next-qrcode"
 export default function TestPage(props){
     const { Canvas } = useQRCode();
     return(
         <>
             <h1>test page</h1>
-            <Canvas 
-                text={'https://class-economy.vercel.app/student_signup/' + props.params.id}
+            {/* <Canvas 
+                text={process.env.NEXT_PUBLIC_URL_API_KEY + props.params.id}
                 width={200}
-            />
+            /> */}
         </>
     )
 }
