f41c7ef109f5ba91b51e767da67600227ce7ecb6
my0990 <my0990@naver.com>
2023-10-07 22:38:51 +0900

    home Link to signup2

--- app/test/[id]/page.js
+++ app/test/[id]/page.js
@@ -6,7 +6,7 @@ export default function TestPage(props){
         <>
             <h1>test page</h1>
             <Canvas 
-                text={'/student_signup/' + props.params.id}
+                text={'https://class-economy.vercel.app/student_signup/' + props.params.id}
                 width={200}
             />
         </>
